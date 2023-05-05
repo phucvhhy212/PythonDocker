cfa3d1e15f7bba973451f2988fdb1107866ad49a
phucvhhy212 <phucvhhy212@gmail.com>
2023-05-05 13:01:45 +0700

    session db

--- app/app.py
+++ app/app.py
@@ -1,70 +1,59 @@
 import json
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, request, Response
+from flask_sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base,sessionmaker
+from sqlalchemy import create_engine
 import os
 import psycopg2
 
 # conn = psycopg2.connect("postgresql://postgres:123456@localhost:5432/postgres")
-url = "postgresql://{user}:{passwd}@{host}:{port}/{db}".format(
-            user=os.environ['USER'], passwd=os.environ['PASSWORD'], host=os.environ['HOST'], port=os.environ['PORT'], db=os.environ['DATABASE']
-        )
+# url = "postgresql://{user}:{passwd}@{host}:{port}/{db}".format(
+#             user=os.environ['USER'], passwd=os.environ['PASSWORD'], host=os.environ['HOST'], port=os.environ['PORT'], db=os.environ['DATABASE']
+#         )
 
-# url = "postgresql://postgres:123456@localhost:5432/postgres"
+url = "postgresql://postgres:123456@localhost:5432/test"
 app = Flask(__name__)
 
-def get_db_connection():
-    conn = psycopg2.connect(url)
-    return conn
 
+Base = declarative_base()
 
-@app.route('/initdb')
-def inittable():
-    conn = psycopg2.connect(url)
-    cur = conn.cursor()
-    cur.execute('DROP TABLE IF EXISTS products;')
-    cur.execute('CREATE TABLE products(id INT PRIMARY KEY,name VARCHAR(50));')
-    cur.execute("INSERT INTO products(id,name) VALUES('1','Laptop')")
-    cur.execute("INSERT INTO products(id,name) VALUES('2','TV')")
-    conn.commit()
-    cur.close()
-    conn.close()
-    return "initialized ! "
+class Products(Base):
+    __tablename__ = 'products'
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    name = Column(String(250),nullable=False)
 
+engine = create_engine(url)
+Base.metadata.create_all(engine)
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
 
 @app.route('/products')
 def getProducts():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM PRODUCTS;')
-    row_headers = [x[0] for x in cur.description] #('id','name')
-
-    
-    products = cur.fetchall()
-    for product in products: #(1,'Laptop')
-        print(product)
+    products = session.query(Products).all()
     json_data = []
+    # row_headers = [x[0] for x in cur.description] #('id','name')
+    row_headers =  [column.key for column in Products.__table__.columns]
     #zip: [(id,1),(name,Laptop)]
     # to dictionary: [{id:1,name:"Laptop"}]
     # smt dictionary == json
     for product in products:
-        json_data.append(dict(zip(row_headers,product)))
-    cur.close()
-    conn.close()
-    return json.dumps(json_data) #convert to JSOn
+        json_data.append(dict(zip(row_headers,[product.id,product.name])))
+    
+    resp = Response(json.dumps(json_data),200,mimetype='application/json')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
-@app.route('/create',methods = ['POST', 'GET'])
+@app.route('/create',methods = 'POST')
 def create():
-    if request.method == 'POST':
-        id = request.form['id']
-        name = request.form['name']
-
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute('INSERT INTO products(id,name)'
-                    'VALUES (%s, %s)',
-                    (id,name))
-        conn.commit()
-        cur.close()
-        conn.close()
-        return redirect('../products',302)
-
-    return render_template('../templates/create.html')
+    row_headers =  [column.key for column in Products.__table__.columns]
+    json_data = []
+    pname = request.form['name']
+    product = Products(name=pname)
+    session.add(product)
+    session.commit()
+    json_data.append(dict(zip(row_headers,[product.id,product.name])))
+    resp = Response(json.dumps(json_data),200,mimetype='application/json')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+app.run()
