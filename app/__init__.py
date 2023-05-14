079062863443528f7e360b96f668656abfc60385
phucvhhy212 <phucvhhy212@gmail.com>
2023-05-14 17:06:00 +0700

    config Flask

--- app/__init__.py
+++ app/__init__.py
@@ -5,23 +5,18 @@ from flask_migrate import Migrate
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import os
-from dotenv import load_dotenv
 
 db = SQLAlchemy()
-load_dotenv()
-url = "postgresql://{user}:{passwd}@{host}:{port}/{db}".format(
-            user=os.getenv('USER'), passwd=os.getenv('PASSWORD'), host=os.getenv('HOST'), port=os.getenv('PORT'), db=os.getenv('DATABASE')
-        )
 
-engine = create_engine(url)
+
+engine = create_engine(os.environ.get('SQLALCHEMY_DATABASE_URI'))
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = url
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object('config.DevConfig')
     db.init_app(app)
     migrate = Migrate(app, db)
     from .models import Products
--- config.py
+++ config.py
@@ -1,11 +1,16 @@
-import os
+from os import environ,path
+from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
+# Specificy a `.env` file containing key/value config values
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
 
 class Config:
-    USER = os.environ.get('USER')
-    PASSWORD = os.environ.get('PASSWORD')
-    HOST = os.environ.get('HOST')
-    PORT = os.environ.get('PORT')
-    DATABASE = os.environ.get('DATABASE')
+    FLASK_APP = environ.get("FLASK_APP")
+    SECRET_KEY = environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+
+class DevConfig(Config):
+    FLASK_ENV = "development"
+    FLASK_DEBUG = True
